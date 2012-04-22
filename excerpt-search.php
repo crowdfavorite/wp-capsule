5a1cb6204feb1a4945cb7a53cc4146974da8ccdb
Alex King <alex@alexking.org>
2012-04-22 11:59:05 -0600

    incremental progress

---
+++ excerpt-search.php
@@ -0,0 +1,4 @@
+<article <?php post_class('search-excerpt'); ?>>
+	<div class="date"><?php the_time(); ?></div>
+	<div class="content"><?php the_excerpt(); ?></div>
+</article>
--- index.php
+++ index.php
@@ -1,22 +1,47 @@
 <!DOCTYPE html>
-<html lang="en">
+<!--[if IE 6]>
+<html id="ie6" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
 <head>
-	<meta charset="utf-8" />
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width" />
 	<title><?php wp_title(); ?></title>
 </head>
 <body>
 
+<header>
+	<form action="" method="get">
+		<input type="text" name="s" value="" />
+		<input type="submit" name="search_submit" value="<?php _e('search', 'exhaust'); ?>" />
+	</form>
+</header>
+
+<div class="body">
 <?php
 
 if (have_posts()) {
-	$i = 0;
 	while (have_posts()) {
 		the_post();
+?>
+	<h2 class="h4"><?php the_date(); ?></h2>
+<?php
 		include('excerpt.php');
 	}
 }
 
 ?>
+</div>
+
+<footer></footer>
 
 </body>
 </html>
