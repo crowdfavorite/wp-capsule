cf5f36b234f34cd2ca28994f621f7c4f5c146413
Alex King <alex@alexking.org>
2012-05-03 17:03:25 -0600

    no longer needed

--- views/excerpt-search.php
+++
@@ -1,5 +0,0 @@
-<article id="post-search-<?php the_ID(); ?>" data-post-id="<?php the_ID(); ?>" <?php post_class('search-excerpt clearfix'); ?>>
-	<div class="date"><?php the_time(); ?></div>
-	<div class="content"><?php the_excerpt(); ?></div>
-	<div class="edit"><?php edit_post_link(__('Edit', 'rutter'), '<span class="edit-link">', '</span>'); ?></div>
-</article>
