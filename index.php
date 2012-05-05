1664b2039f6f23d92690c245c3a5d067e0b8f978
Alex King <alex@alexking.org>
2012-05-05 09:12:18 -0600

    basic pagination added

--- index.php
+++ index.php
@@ -82,6 +82,15 @@ if (have_posts()) {
 			include('views/excerpt.php');
 		}
 	}
+	if ( $wp_query->max_num_pages > 1 ) {
+?>
+		<nav class="pagination">
+			<div class="nav-previous"><?php next_posts_link( __( 'Older posts <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?></div>
+			<div class="nav-next"><?php previous_posts_link( __( '<span class="meta-nav">&larr;</span> Newer posts', 'twentyeleven' ) ); ?></div>
+		</nav>
+<?php
+	}
+
 }
 
 ?>
