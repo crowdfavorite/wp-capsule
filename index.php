925cb51ceb62066aac7ab6a4ea432c57f9c086a3
Alex King <alex@alexking.org>
2012-05-05 09:15:46 -0600

    basic pagination styles

--- index.php
+++ index.php
@@ -84,7 +84,7 @@ if (have_posts()) {
 	}
 	if ( $wp_query->max_num_pages > 1 ) {
 ?>
-		<nav class="pagination">
+		<nav class="pagination clearfix">
 			<div class="nav-previous"><?php next_posts_link( __( 'Older posts <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?></div>
 			<div class="nav-next"><?php previous_posts_link( __( '<span class="meta-nav">&larr;</span> Newer posts', 'twentyeleven' ) ); ?></div>
 		</nav>
