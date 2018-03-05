a9d64c0ab6736dcfb726b4c81005f2f314d309b2
Cristi Rusu <cristi@crowdfavorite.com>
2018-03-05 15:36:58 +0200

    git subrepo pull --branch=develop ui
    subrepo:
      subdir:   "ui"
      merged:   "40a9c44"
    upstream:
      origin:   "git@github.com:crowdfavorite/wp-capsule-ui.git"
      branch:   "develop"
      commit:   "40a9c44"
    git-subrepo:
      version:  "0.4.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "74339e8"

--- ui/views/deleted.php
+++ ui/views/deleted.php
@@ -1,3 +1,13 @@
-<article id="post-deleted-<?php echo $post->ID; ?>" data-post-id="<?php echo $post->ID; ?>" <?php post_class('deleted clearfix', $post->ID); ?>>
-	<div class="post-content"><?php printf(__('<span class="msg">Post successfully deleted.</span> <a href="%s" class="post-undelete-link">Recover from Trash</a>', 'capsule'), admin_url('edit.php?post_status=trash&post_type=post')); ?></div>
+<?php
+/**
+ * Deleted post template.
+ *
+ * @package capsule
+ */
+
+?>
+<article id="post-deleted-<?php echo (int) $post->ID; ?>" data-post-id="<?php echo (int) $post->ID; ?>" <?php post_class( 'deleted clearfix', $post->ID ); ?>>
+	<div class="post-content">
+		<span class="msg"><?php esc_html_e( 'Post successfully deleted.', 'capsule' ); ?></span> <a href="<?php echo esc_url( admin_url( 'edit.php?post_status=trash&post_type=post' ) ); ?>" class="post-undelete-link"><?php esc_html_e( 'Recover from Trash', 'capsule' ); ?></a>
+	</div>
 <article>
--- ui/views/excerpt.php
+++ ui/views/excerpt.php
@@ -1,56 +1,56 @@
-<article id="post-content-<?php the_ID(); ?>" data-post-id="<?php the_ID(); ?>" <?php post_class('content clearfix excerpt' . (is_sticky() ? ' sticky' : '')); ?>>
+<?php
+/**
+ * Post excerpt template.
+ *
+ * @package capsule
+ */
+
+$pushed            = '';
+$is_capsule_server = is_capsule_server();
+if ( ! $is_capsule_server ) {
+	$pushed = capsule_last_pushed( get_the_ID() );
+}
+
+?>
+<article id="post-content-<?php the_ID(); ?>" data-post-id="<?php the_ID(); ?>" <?php post_class( 'content clearfix excerpt' . ( is_sticky() ? ' sticky' : '' ) ); ?>>
 	<div class="post-date">
 		<a href="<?php the_permalink(); ?>">
 			<ul>
-				<li class="day"><?php the_time('j'); ?></li>
-				<li class="month"><?php the_time('M'); ?></li>
-				<li class="year"><?php the_time('Y'); ?></li>
+				<li class="day"><?php the_time( 'j' ); ?></li>
+				<li class="month"><?php the_time( 'M' ); ?></li>
+				<li class="year"><?php the_time( 'Y' ); ?></li>
 			</ul>
 		</a>
-<?php
-if (!is_capsule_server()) {
-	$pushed = capsule_last_pushed(get_the_ID());
-	if (!empty($pushed)) {
-?>
+		<?php if ( ! empty( $pushed ) ) : ?>
 		<div class="push-server-meta">
 			<span class="trigger">&#59254;</span>
-			<?php echo $pushed; ?>
+			<?php echo wp_kses_post( $pushed ); ?>
 		</div>
-<?php
-	}
-}
-?>
+		<?php endif; ?>
 	</div>
 	<div class="post-meta">
-<?php
-echo capsule_term_list(get_the_ID(), 'projects');
-echo capsule_term_list(get_the_ID(), 'post_tag');
-echo capsule_term_list(get_the_ID(), 'code');
-
-if (is_capsule_server()) {
-?>
+	<?php
+		echo wp_kses_post( capsule_term_list( get_the_ID(), 'projects' ) );
+		echo wp_kses_post( capsule_term_list( get_the_ID(), 'post_tag' ) );
+		echo wp_kses_post( capsule_term_list( get_the_ID(), 'code' ) );
+	?>
+	<?php if ( is_capsule_server() ) : ?>
 		<p class="author">
-<?php
-
-echo get_avatar(get_the_author_meta('email'), 20);
-printf(__('<a href="%1$s">%2$s</a>', 'capsule'), esc_url(get_author_posts_url($post->post_author)), get_the_author_meta('display_name'));
-
-?>
+			<?php echo get_avatar( get_the_author_meta( 'email' ), 20 ); ?>
+			<a href="<?php echo esc_url( get_author_posts_url( $post->post_author ) ); ?>"><?php echo esc_html( get_the_author_meta( 'display_name' ) ); ?></a>
 		</p>
-<?php
-}
-?>
+	<?php endif; ?>
 	</div>
 	<div class="post-content">
 		<?php the_content(); ?>
 		<nav class="post-menu">
-			<?php do_action('capsule_post_menu_before', get_the_ID()); ?>
-			<a href="#" class="post-sticky-link" title="<?php _e('Star', 'capsule'); ?>">&#57391;</a>
-			<a href="#" class="post-unsticky-link" title="<?php _e('Un-Star', 'capsule'); ?>">&#57393;</a>
-			<span class="post-sticky-loading" title="<?php _e('Loading...', 'capsule'); ?>">&#59441;</span>
-			<?php edit_post_link('&#57535;', '', ''); ?>
-			<a href="#" class="post-delete-link" title="<?php _e('Trash', 'capsule'); ?>">&#59177;</a>
-			<?php do_action('capsule_post_menu_after', get_the_ID()); ?>
+			<?php do_action( 'capsule_post_menu_before', get_the_ID() ); ?>
+			<a href="#" class="post-sticky-link" title="<?php esc_html_e( 'Star', 'capsule' ); ?>">&#57391;</a>
+			<a href="#" class="post-unsticky-link" title="<?php esc_html_e( 'Un-Star', 'capsule' ); ?>">&#57393;</a>
+			<span class="post-sticky-loading" title="<?php esc_html_e( 'Loading...', 'capsule' ); ?>">&#59441;</span>
+			<?php edit_post_link( '&#57535;', '', '' ); ?>
+			<a href="#" class="post-delete-link" title="<?php esc_html_e( 'Trash', 'capsule' ); ?>">&#59177;</a>
+			<?php do_action( 'capsule_post_menu_after', get_the_ID() ); ?>
 		</nav>
 		<div class="post-toggle"></div>
 	</div>
