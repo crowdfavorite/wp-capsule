d6fbd7b3daeaac4029241eed8393426e08c73d69
Al N <anitu@crowdfavorite.com>
2020-04-30 14:36:31 +0300

    remove ui folder to add it as a subtree

--- ui/lib/ace/build/src-noconflict/snippets/redshift.js
+++
@@ -1,7 +0,0 @@
-ace.define("ace/snippets/redshift",["require","exports","module"], function(require, exports, module) {
-"use strict";
-
-exports.snippetText =undefined;
-exports.scope = "redshift";
-
-});
--- ui/lib/phpjs/functions/var/is_bool.js
+++
@@ -1,9 +0,0 @@
-function is_bool (mixed_var) {
-    // http://kevin.vanzonneveld.net
-    // +   original by: Onno Marsman
-    // *     example 1: is_bool(false);
-    // *     returns 1: true
-    // *     example 2: is_bool(0);
-    // *     returns 2: false
-    return (typeof mixed_var === 'boolean');
-}
