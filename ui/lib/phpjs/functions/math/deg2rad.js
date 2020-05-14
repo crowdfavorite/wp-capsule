c13a1c136c9a7454a229f31e2ae43352128cc078
Marian Bucur <mbucur@crowdfavorite.com>
2020-05-14 14:35:30 +0300

    Removed ui directory.

--- ui/lib/phpjs/functions/math/deg2rad.js
+++
@@ -1,7 +0,0 @@
-function deg2rad (angle) {
-    // http://kevin.vanzonneveld.net
-    // +   original by: Enrique Gonzalez
-    // *     example 1: deg2rad(45);
-    // *     returns 1: 0.7853981633974483
-    return (angle / 180) * Math.PI;
-}
